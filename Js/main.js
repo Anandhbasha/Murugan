0e76328dd9a1e37ed43a759111ed17a78b886efa
Anandhakumar <anandhaanavamani@gmail.com>
2026-06-15 12:28:44 +0530

    variable

---
+++ Js/main.js
@@ -0,0 +1,31 @@
+// Hoisting
+// var 
+// console.log("The a value is:",a);
+// var a=10
+// console.log("The a value is:",a);
+//sep-> 
+// console.log("Apple","banana");
+
+// var a =20 //redeclaration
+// a =50 //re-assign
+// console.log("The a value is:",a);
+// // let 
+// console.log("The a value is:",a);
+// let a =50
+// a =60
+// console.log("The a value is:",a);
+
+
+// const
+// const a = 50
+// a=80
+// console.log(a);
+let a =60 
+{
+    //local scope->let and const
+    console.log(a);
+}
+// scope 
+// global ->{outside of the block or else var is also called as global scope}
+// local scope ->{ inside the block}
+
